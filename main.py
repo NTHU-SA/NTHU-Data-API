9bf852a15325d4135bbf1e276fa0c4e89455ca89
Wei <kiwi072400.lin@gmail.com>
2023-10-14 15:06:33 +0800

    fix: 修正端口與環境設定

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         uvicorn.run(
             "src:app",
             host="0.0.0.0",
-            port=int(os.getenv("PORT")),
+            port=int(os.getenv("PORT") or 5000),
             log_level="debug",
             reload=True,  #  reload the server every time code changes
             ssl_keyfile="src/utils/openssl/key.pem",
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         uvicorn.run(
             "src:app",
             host="0.0.0.0",
-            port=int(os.getenv("PORT")),
+            port=int(os.getenv("PORT") or 5000),
             log_level="error",
             workers=2,
             ssl_keyfile="src/utils/openssl/key.pem",
