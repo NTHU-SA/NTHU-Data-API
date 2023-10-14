b63c6049dec526becdbfc089c4b3307e75b85484
iwtba4188 <X69599596@gmail.com>
2023-10-14 23:11:48 +0800

    feat: 新增轉換phone和location的formatter - original_data/dept.json裡面的「事務性單位」下，原本是people的資料，手動改至departments，之後如果要 重新匯出要注意（當然上線之後不會再匯出資料，會以其他方式修改，避免產生新的id）。

---
+++ data/format_data/scripts/data_node.py
@@ -0,0 +1,16 @@
+import uuid
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+
+class DataNode:
+    def __init__(self) -> None:
+        self.id = str(uuid.uuid4())
+        self.data = {}
+        self.create_time = str(self._gen_now_timestamp())
+        self.update_time = str(self._gen_now_timestamp())
+
+    def _gen_now_timestamp(self):
+        return datetime.now(tz=ZoneInfo("Asia/Taipei")).strftime("%Y%m%dT%H%M%S%z")
+
+a = DataNode()
---
+++ data/format_data/scripts/phone_data_node.py
@@ -0,0 +1,15 @@
+from data_node import DataNode
+
+
+class PhoneDataNode(DataNode):
+    def __init__(self, name: str, ext: str, tel: str, fax: str, email: str) -> None:
+        super().__init__()
+        self.data = {
+            "name": name,
+            "ext": ext,
+            "tel": tel,
+            "fax": fax,
+            "email": email,
+            "parents": [],
+            "children": [],
+        }
