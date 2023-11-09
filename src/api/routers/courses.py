363ed77d70e539c35894ca077c3a800a216108e6
l7wei <l7wei@users.noreply.github.com>
2023-11-09 08:26:07 +0000

    style: Format code with black and isort

--- src/api/routers/courses.py
+++ src/api/routers/courses.py
@@ -1,7 +1,8 @@
 from enum import Enum
 from typing import Union
-from fastapi import APIRouter, HTTPException, Path, Query, Body
-from pydantic import BaseModel, RootModel, field_validator, Field
+
+from fastapi import APIRouter, Body, HTTPException, Path, Query
+from pydantic import BaseModel, Field, RootModel, field_validator
 
 from ..models.courses import Conditions, Processor
 
