9fc4423b544197715d3d71d20a16fa827be83638
Wei <87221670+l7wei@users.noreply.github.com>
2023-11-14 19:32:57 +0800

    feat: 更新 dependencies 版本，改善 API 路徑 (#24)
    * build(deps): bump uvicorn from 0.23.2 to 0.24.0.post1
    
    Bumps [uvicorn](https://github.com/encode/uvicorn) from 0.23.2 to 0.24.0.post1.
    - [Release notes](https://github.com/encode/uvicorn/releases)
    - [Changelog](https://github.com/encode/uvicorn/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/encode/uvicorn/compare/0.23.2...0.24.0.post1)
    
    ---
    updated-dependencies:
    - dependency-name: uvicorn
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * build(deps): bump pandas from 2.1.2 to 2.1.3
    
    Bumps [pandas](https://github.com/pandas-dev/pandas) from 2.1.2 to 2.1.3.
    - [Release notes](https://github.com/pandas-dev/pandas/releases)
    - [Commits](https://github.com/pandas-dev/pandas/compare/v2.1.2...v2.1.3)
    
    ---
    updated-dependencies:
    - dependency-name: pandas
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * feat: 改善 API 路徑
    
    將 /rpage 改成 /scrapers/rpage/announcements
    將 /buses 裡面的 information 改成 info
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/api/routers/buses.py
+++ src/api/routers/buses.py
@@ -16,19 +16,7 @@ async def get_main():
     return buses.get_main_data()
 
 
-@router.get(
-    "/main/information/toward_tsmc_building_info", response_model=schemas.buses.BusInfo
-)
-async def get_main_gate_toward_tsmc_building_info() -> schemas.buses.BusInfo:
-    """
-    校門口往台積館公車資訊。
-    """
-    return buses.get_main_data()["toward_TSMC_building_info"]
-
-
-@router.get(
-    "/main/information/toward_main_gate_info", response_model=schemas.buses.BusInfo
-)
+@router.get("/main/info/toward_main_gate", response_model=schemas.buses.BusInfo)
 async def get_tsmc_building_toward_main_gate_info() -> schemas.buses.BusInfo:
     """
     台積館往校門口公車資訊。
@@ -36,17 +24,12 @@ async def get_tsmc_building_toward_main_gate_info() -> schemas.buses.BusInfo:
     return buses.get_main_data()["toward_main_gate_info"]
 
 
-@router.get(
-    "/main/schedules/weekday/toward_tsmc_building",
-    response_model=list[schemas.buses.BusMainSchedule],
-)
-async def get_main_weekday_bus_schedule_toward_tsmc_building() -> list[
-    schemas.buses.BusMainSchedule
-]:
+@router.get("/main/info/toward_tsmc_building", response_model=schemas.buses.BusInfo)
+async def get_main_gate_toward_tsmc_building_info() -> schemas.buses.BusInfo:
     """
-    校門口往台積館公車時刻表（平日）。
+    校門口往台積館公車資訊。
     """
-    return buses.get_main_data()["weekday_bus_schedule_toward_TSMC_building"]
+    return buses.get_main_data()["toward_TSMC_building_info"]
 
 
 @router.get(
@@ -63,16 +46,16 @@ async def get_main_weekday_bus_schedule_toward_main_gate() -> list[
 
 
 @router.get(
-    "/main/schedules/weekend/toward_tsmc_building",
+    "/main/schedules/weekday/toward_tsmc_building",
     response_model=list[schemas.buses.BusMainSchedule],
 )
-async def get_main_weekend_bus_schedule_toward_tsmc_building() -> list[
+async def get_main_weekday_bus_schedule_toward_tsmc_building() -> list[
     schemas.buses.BusMainSchedule
 ]:
     """
-    校門口往台積館公車時刻表（假日）。
+    校門口往台積館公車時刻表（平日）。
     """
-    return buses.get_main_data()["weekend_bus_schedule_toward_TSMC_building"]
+    return buses.get_main_data()["weekday_bus_schedule_toward_TSMC_building"]
 
 
 @router.get(
@@ -88,6 +71,19 @@ async def get_main_weekend_bus_schedule_toward_main_gate() -> list[
     return buses.get_main_data()["weekend_bus_schedule_toward_main_gate"]
 
 
+@router.get(
+    "/main/schedules/weekend/toward_tsmc_building",
+    response_model=list[schemas.buses.BusMainSchedule],
+)
+async def get_main_weekend_bus_schedule_toward_tsmc_building() -> list[
+    schemas.buses.BusMainSchedule
+]:
+    """
+    校門口往台積館公車時刻表（假日）。
+    """
+    return buses.get_main_data()["weekend_bus_schedule_toward_TSMC_building"]
+
+
 ####################################################################################################
 @router.get("/nanda", response_model=schemas.buses.BusNandaData)
 async def get_nanda():
@@ -97,9 +93,7 @@ async def get_nanda():
     return buses.get_nanda_data()
 
 
-@router.get(
-    "/nanda/information/toward_main_campus", response_model=schemas.buses.BusInfo
-)
+@router.get("/nanda/info/toward_main_campus", response_model=schemas.buses.BusInfo)
 async def get_nanda_toward_main_campus_info() -> schemas.buses.BusInfo:
     """
     南大往本部區間車資訊。
@@ -107,9 +101,7 @@ async def get_nanda_toward_main_campus_info() -> schemas.buses.BusInfo:
     return buses.get_nanda_data()["toward_main_campus_info"]
 
 
-@router.get(
-    "/nanda/information/toward_south_campus", response_model=schemas.buses.BusInfo
-)
+@router.get("/nanda/info/toward_south_campus", response_model=schemas.buses.BusInfo)
 async def get_nanda_toward_south_campus_info() -> schemas.buses.BusInfo:
     """
     本部往南大區間車資訊。
