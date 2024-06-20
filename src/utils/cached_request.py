208fd32091d56486f3bf448fefdca6f9f4e28508
iwtba4188 <X69599596@gmail.com>
2024-06-20 15:35:22 +0800

    feat(utils): 更改快取方法，改為通用快取而非原先 TTL
    - 配合其他 models 需使用內容快取，來確保取得資料失敗時，至少有一個資料可用
    - 這項改動會影響到原先 scrapers 的快取，使其快取失效，如需新增 TTL cache 加快速度，需額外使用 fastapi-cache package 或對端口加掛 TTLCache

--- src/utils/cached_request.py
+++ src/utils/cached_request.py
@@ -1,10 +1,10 @@
 from urllib.parse import urlparse, urlunparse
 
 import requests
-from cachetools import TTLCache
+from cachetools import Cache
 from fastapi import HTTPException
 
-ttl_cache = TTLCache(maxsize=1024, ttl=60 * 60)
+cache = Cache(maxsize=1024)
 
 
 def generate_headers(referer: str) -> dict:
@@ -25,15 +25,31 @@ def generate_headers(referer: str) -> dict:
 
 
 def validate_url(url: str) -> str:
+    """
+    驗證網址是否使用 http 和 https 協議，且僅為 nthu.edu.tw 網域。
+
+    Args:
+        url (str): 網址。
+    Returns:
+        str: 經過驗證的網址。
+    Raises:
+        ValueError: 當網址不合法時。
+    """
+
     parsed_url = urlparse(url)
+
+    # 確認 URL 是否包含 scheme 和 netloc
     if not parsed_url.scheme or not parsed_url.netloc:
         raise ValueError("Invalid URL")
+
     # 只允許 http 或 https 協議
     if parsed_url.scheme not in ["http", "https"]:
         raise ValueError("Invalid URL scheme")
+
     # 只允許 nthu.edu.tw 結尾的網域
     if not parsed_url.netloc.endswith("nthu.edu.tw"):
         raise ValueError("Invalid URL domain")
+
     # 重新組合 URL，排除可能的用戶名和密碼
     safe_url = urlunparse(
         (
@@ -45,22 +61,39 @@ def validate_url(url: str) -> str:
             parsed_url.fragment,
         )
     )
+
     return safe_url
 
 
 def delete(url: str) -> None:
-    if url in ttl_cache:
-        del ttl_cache[url]
-
-
-def get(url: str, cache=True, update=False, auto_headers=True, **kwargs) -> str:
     """
-    從網址取得回應內容。
+    刪除快取中的網址（若有）。
+
     Args:
         url (str): 網址。
-        cache (bool): 是否使用快取。
+    """
+
+    if url in cache:
+        del cache[url]
+
+
+def get(
+    url: str,
+    update: bool = False,
+    auto_headers: bool = True,
+    **kwargs,
+) -> tuple[str, bool]:
+    """
+    從網址取得回應內容。
+
+    Args:
+        url (str): 網址。
         update (bool): 是否更新快取。
+        auto_headers (bool): 是否自動生成 headers。
         **kwargs: 額外傳遞給的參數，會傳遞給 requests.get。
+
+    Returns:
+        tuple[str, bool]: 回應內容和是否使用快取。
     """
 
     # 驗證網址
@@ -69,37 +102,59 @@ def get(url: str, cache=True, update=False, auto_headers=True, **kwargs) -> str:
     except ValueError as e:
         raise HTTPException(400, str(e))
 
-    if update and url in ttl_cache:
-        del ttl_cache[url]
-    if cache and url in ttl_cache:
-        return ttl_cache[url]
-    if auto_headers is True:
-        headers = generate_headers(url)
-    else:
-        headers = None
-    response = requests.get(url, headers, timeout=10, **kwargs)
-    status_code = response.status_code
-    if status_code != 200:
-        raise HTTPException(status_code, f"Request error: {status_code}")
-    response_text = response.text
-    if cache:
-        ttl_cache[url] = response_text
-    return response_text
+    # 如果不更新快取且有先前快取，則直接回傳快取內容
+    if not update and url in cache:
+        return cache[url], True
+
+    # 若無快取或需更新快取，則重新發送請求
+    try:
+        headers = generate_headers(url) if auto_headers else None
+
+        response = requests.get(url, headers, timeout=10, **kwargs)
+
+        status_code = response.status_code
+        # 如果回應狀態碼不是 200，則拋出例外；但有可能是 2xx 範圍的狀態碼
+        if status_code != 200:
+            raise HTTPException(status_code, f"Request error: {status_code}")
+
+        response_text = response.text
+        cache[url] = response_text
+
+        return cache[url], False
+
+    # 捕捉 requests.get 丟出的例外，如果有先前快取，則回傳快取內容，否則拋出例外
+    except HTTPException as e:
+        if url in cache:
+            return cache[url], True
+        raise e
 
 
-def post(url: str, cache=True, update=False, **kwargs) -> str:
+def post(
+    url: str,
+    **kwargs,
+) -> str:
     """
-    從網址取得回應內容。
+    從網址取得回應內容。Post 方法不使用快取。
+
+    Args:
+        url (str): 網址。
+        **kwargs: 額外傳遞給的參數，會傳遞給 requests.get。
+
+    Returns:
+        str: 回應內容。
     """
-    if update and url in ttl_cache:
-        del ttl_cache[url]
-    if cache and url in ttl_cache:
-        return ttl_cache[url]
+
+    # 驗證網址
+    try:
+        url = validate_url(url)
+    except ValueError as e:
+        raise HTTPException(400, str(e))
+
     response = requests.post(url, timeout=10, **kwargs)
+
     status_code = response.status_code
+    # 如果回應狀態碼不是 200，則拋出例外；但有可能是 2xx 範圍的狀態碼
     if status_code != 200:
         raise HTTPException(status_code, f"Request error: {status_code}")
-    response_text = response.text
-    if cache:
-        ttl_cache[url] = response_text
-    return response_text
+
+    return response.text
