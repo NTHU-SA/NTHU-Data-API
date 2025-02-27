7c383a6d78a3d6986c3ca5dff0ec29d6e7370912
Wei <kiwi072400.lin@gmail.com>
2025-02-27 21:47:04 +0800

    feat: combine search function

--- src/api/routers/departments.py
+++ src/api/routers/departments.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Dict, List, Union
 
 import requests
 from fastapi import APIRouter, HTTPException, Query
@@ -7,7 +7,7 @@ from thefuzz import fuzz
 from src.api.models.departments import Department, Person
 
 router = APIRouter()
-departments_data = requests.get("https://data.nthusa.tw/directory.json").json()
+directory_data = requests.get("https://data.nthusa.tw/directory.json").json()
 
 
 @router.get("/", response_model=List[Department], summary="取得所有部門列表")
@@ -15,61 +15,49 @@ async def read_all_departments():
     """
     取得所有部門列表。
     """
-    return departments_data
+    return directory_data
 
 
 @router.get(
-    "/fuzzy-search",
-    response_model=List[Department],
-    summary="模糊搜尋部門名稱",
+    "/search",
+    response_model=Dict[str, Union[List[Department], List[Person]]],
+    summary="關鍵字搜尋部門與人員名稱",
 )
 async def fuzzy_search_departments(
-    name: str = Query(..., example="校長", title="部門名稱模糊關鍵字"),
+    name: str = Query(..., example="校長", title="模糊搜尋關鍵字"),
 ):
     """
-    使用模糊搜尋演算法搜尋部門名稱，找出最相似的部門。
+    使用搜尋演算法搜尋部門與人員名稱。
     """
-    results = []
-    for dept in departments_data:
-        similarity = fuzz.partial_ratio(
-            name, dept["name"]
-        )  # 使用 partial_ratio 進行模糊比對
+    department_results = []
+    for dept in directory_data:
+        similarity = fuzz.partial_ratio(name, dept["name"])
         if similarity >= 60:  # 相似度門檻值，可以調整
-            dept["similarity_score"] = similarity  # 可以加入相似度分數方便排序
-            results.append(dept)
-
-    results.sort(
+            dept["similarity_score"] = similarity  # 加入相似度分數方便排序
+            department_results.append(dept)
+    department_results.sort(
         key=lambda x: x.get("similarity_score", 0), reverse=True
     )  # 根據相似度排序
-    return results
 
-
-@router.get(
-    "/{index}", response_model=List[Department], summary="依據 index 取得特定部門"
-)  # 需要把它往下移，不然 fuzzy-search 會被擋住
-async def read_department_by_index(index: str):
-    for dept in departments_data:
-        if dept["index"] == index:
-            return [dept]
-    raise HTTPException(status_code=404, detail="Department not found")
-
-
-@router.get(
-    "/people/fuzzy-search", response_model=List[Person], summary="模糊搜尋人員名稱"
-)
-async def fuzzy_search_people(
-    name: str = Query(..., example="高為元", title="人員名稱模糊關鍵字")
-):
-    """
-    使用模糊搜尋演算法搜尋人員名稱，找出最相似的人員 (跨部門)。
-    """
-    results = []
-    for dept in departments_data:
+    people_results = []
+    for dept in directory_data:
         for person in dept["details"]["people"]:
             similarity = fuzz.partial_ratio(name, person["name"])
             if similarity >= 70:  # 相似度門檻值，可以調整
                 person["similarity_score"] = similarity
-                results.append(person)
+                people_results.append(person)
+    people_results.sort(
+        key=lambda x: x.get("similarity_score", 0), reverse=True
+    )  # 根據相似度排序
 
-    results.sort(key=lambda x: x.get("similarity_score", 0), reverse=True)
-    return results
+    return {"departments": department_results, "people": people_results}
+
+
+@router.get(
+    "/{index}", response_model=List[Department], summary="依據 index 取得特定部門"
+)  # 需要把它往下移，不然 search 會被擋住
+async def read_department_by_index(index: str):
+    for dept in directory_data:
+        if dept["index"] == index:
+            return [dept]
+    raise HTTPException(status_code=404, detail="Department not found")
