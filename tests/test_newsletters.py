af165792e3cd11f5e4809da338091acbe1318d21
Wei <kiwi072400.lin@gmail.com>
2025-03-02 23:19:45 +0800

    fix: 更改 test newsletter 路徑

--- tests/test_newsletters.py
+++ tests/test_newsletters.py
@@ -13,7 +13,7 @@ def test_newsletter():
 
 
 @pytest.mark.parametrize(
-    "newsletter_name", [_.value for _ in schemas.newsletter.NewsletterName]
+    "newsletter_name", [_.value for _ in schemas.newsletters.NewsletterName]
 )
 def test_newsletter_searches(newsletter_name):
     response = client.get(url=f"/newsletters/{newsletter_name}")
