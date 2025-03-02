99f3edbbbd760b1862a9eaec9ff5e029b197aaa9
Wei <kiwi072400.lin@gmail.com>
2025-03-02 22:54:22 +0800

    feat: 新增 announcements test

---
+++ tests/test_announcements.py
@@ -0,0 +1,23 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from src import app
+
+client = TestClient(app)
+department_list = ["清華公佈欄", "國立清華大學學生會"]
+
+
+def test_announcements_endpoints():
+    response = client.get(url="/announcements/")
+    assert response.status_code == 200
+
+
+def test_get_all_department():
+    response = client.get(url="/announcements/department")
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize("name", department_list)
+def test_get_announcements_by_department(name):
+    response = client.get(url=f"/announcements/department/{name}")
+    assert response.status_code == 200
