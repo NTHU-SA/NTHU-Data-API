d0eb116c089a6e62704b41061c4e5418618557db
Wei <kiwi072400.lin@gmail.com>
2025-03-03 14:01:05 +0800

    refactor: 合併 resources 至 libraries 中

--- src/api/schemas/libraries.py
+++ src/api/schemas/libraries.py
@@ -1,6 +1,31 @@
 from enum import Enum
+from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl
+
+
+class RssImage(BaseModel):
+    # url 使用 str 而非 HttpUrl，因為有些圖片的 url 並非合法的 url，例如: //www.lib.nthu.edu.tw/image/news/8/20230912.jpg
+    url: str = Field(..., description="圖片網址")
+    title: str = Field(..., description="圖片標題")
+    link: Optional[HttpUrl] = Field(..., description="連結")
+
+
+class RssItem(BaseModel):
+    guid: str = Field(..., description="文章 id")
+    category: str = Field(..., description="文章分類")
+    title: str = Field(..., description="文章標題")
+    link: Optional[HttpUrl] = Field(..., description="文章連結")
+    pubDate: str = Field(..., description="文章發布日期")
+    description: str = Field(..., description="文章內容")
+    author: str = Field(..., description="文章作者")
+    image: RssImage = Field(..., description="文章圖片")
+
+
+class RssData(BaseModel):
+    title: Optional[str] = Field(..., description="電子報標題")
+    link: Optional[HttpUrl] = Field(..., description="電子報網址")
+    date: Optional[str] = Field(..., description="發布日期")
 
 
 class LibraryName(str, Enum):
